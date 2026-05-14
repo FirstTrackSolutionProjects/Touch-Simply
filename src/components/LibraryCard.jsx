a088cf266e9ddb05c3b4f18596ed69730ca907c1
sOuL2000s <souparnopaulreborn@gmail.com>
2026-05-14 20:03:55 +0530

    error small

--- src/components/LibraryCard.jsx
+++ src/components/LibraryCard.jsx
@@ -1,17 +1,3 @@
-import React from "react";
-
-import { Link } from "react-router-dom";
-
-import {
-  Trash2,
-  Download,
-  FileText,
-  Image as ImageIcon,
-  Presentation,
-  Briefcase,
-  Pencil,
-} from "lucide-react";
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {
